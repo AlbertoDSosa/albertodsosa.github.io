0baf21cc42a0d1b04c2b0face74065781f83234e
Alberto D.Sosa <albertodsosa@gmail.com>
2019-05-31 11:13:51 +0200

    Initial Commit

---
+++ precache-manifest.8d466916477cdcb3d784b2f7abb9071a.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427b7756fff26704d845f0e615750bf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf9678596408cceee84b",
+    "url": "/static/css/main.33592ab2.chunk.css"
+  },
+  {
+    "revision": "82b17b566a3690b5f6ae",
+    "url": "/static/js/2.282ae6f6.chunk.js"
+  },
+  {
+    "revision": "bf9678596408cceee84b",
+    "url": "/static/js/main.e99938fe.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "72c742bc4398b2803e5ebcdc65d4a03c",
+    "url": "/static/media/IcoMoon-Free.72c742bc.ttf"
+  },
+  {
+    "revision": "6908ab0d7dceb81a70c0f68a35012fc1",
+    "url": "/static/media/angular.6908ab0d.png"
+  },
+  {
+    "revision": "a63ade5bcf9e852a9b1e2a5fa1e90ccb",
+    "url": "/static/media/browserify.a63ade5b.png"
+  },
+  {
+    "revision": "60aaf6431d012933d249b05a41b4f54f",
+    "url": "/static/media/css3.60aaf643.png"
+  },
+  {
+    "revision": "42b5988ec2c4d44ba4684daf664cecc4",
+    "url": "/static/media/databases.42b5988e.png"
+  },
+  {
+    "revision": "406645e789dfce91d0e7c5c1f6a98aac",
+    "url": "/static/media/docker.406645e7.png"
+  },
+  {
+    "revision": "ecba0b66bbee9d6cf4e75c4aef3ce80e",
+    "url": "/static/media/express.ecba0b66.png"
+  },
+  {
+    "revision": "79a1c4821f023ba1efa240b12994b892",
+    "url": "/static/media/geekshubs.79a1c482.jpg"
+  },
+  {
+    "revision": "54d871a113dc4ee59c41600da1345f57",
+    "url": "/static/media/gitflow.54d871a1.png"
+  },
+  {
+    "revision": "3db4f2f86273d3b0491c28e5b46fe796",
+    "url": "/static/media/gulp.3db4f2f8.png"
+  },
+  {
+    "revision": "46c8784ca5a359cf9137f52c43e8dd30",
+    "url": "/static/media/header-img.46c8784c.png"
+  },
+  {
+    "revision": "5150c3a0e082ae7accf9e61ec4f0b753",
+    "url": "/static/media/javascript.5150c3a0.png"
+  },
+  {
+    "revision": "e2fb011574bae3f84cadead411e9db8e",
+    "url": "/static/media/jquery.e2fb0115.png"
+  },
+  {
+    "revision": "2956df8476b56b1fc67c367910d1c156",
+    "url": "/static/media/laravel.2956df84.png"
+  },
+  {
+    "revision": "85dc556dfee506ee683a5a09f7d02ed1",
+    "url": "/static/media/linux.85dc556d.png"
+  },
+  {
+    "revision": "5e2944cb14bda4c40ea47a2e4d052cd6",
+    "url": "/static/media/mongodb.5e2944cb.png"
+  },
+  {
+    "revision": "c09d1eca8289230efac87536120adce0",
+    "url": "/static/media/mysql.c09d1eca.png"
+  },
+  {
+    "revision": "0bf506915f3ce389465a64976e718505",
+    "url": "/static/media/nginx.0bf50691.png"
+  },
+  {
+    "revision": "5271f005bc48789c94f270789ca150c1",
+    "url": "/static/media/nodejs.5271f005.png"
+  },
+  {
+    "revision": "e3a266ed08a9a1e1fa8d37135321b449",
+    "url": "/static/media/php.e3a266ed.png"
+  },
+  {
+    "revision": "87f1b07c8592e8f55092f747528ccf70",
+    "url": "/static/media/reactjs.87f1b07c.png"
+  },
+  {
+    "revision": "e781e5831b873e2443ee235405b39032",
+    "url": "/static/media/redux.e781e583.png"
+  },
+  {
+    "revision": "b6780715fd868eafd6b77e4b09d570df",
+    "url": "/static/media/sass.b6780715.png"
+  },
+  {
+    "revision": "cfffbb8a565d8d61698f7b4d40765bf7",
+    "url": "/static/media/school.cfffbb8a.png"
+  },
+  {
+    "revision": "d3aca7e6df08398a8824ffb1ec59e948",
+    "url": "/static/media/stylus.d3aca7e6.png"
+  },
+  {
+    "revision": "b63f4c527d426e0dec5f8e6c68373d8f",
+    "url": "/static/media/typescript.b63f4c52.png"
+  },
+  {
+    "revision": "e8022d3b8e6f3da2423bcc1d655f1f78",
+    "url": "/static/media/webpack.e8022d3b.png"
+  },
+  {
+    "revision": "88968c072b24db86045404f53df35f42",
+    "url": "/static/media/wordpress.88968c07.png"
+  }
+]);
